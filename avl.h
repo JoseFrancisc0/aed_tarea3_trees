495c995e6cf706984127029c20a26226990717e6
JoseFrancisc0 <jose.wong@utec.edu.pe>
2024-01-23 12:55:52 -0500

    Initialized class header files

---
+++ avl.h
@@ -0,0 +1,7 @@
+#ifndef AVL_H
+#define AVL_H
+
+template<typename TK, typename TV>
+class AVL{};
+
+#endif //AVL_H
---
+++ bst.h
@@ -0,0 +1,7 @@
+#ifndef BST_H
+#define BST_H
+
+template<typename TK, typename TV>
+class BST{};
+
+#endif //BST_H
