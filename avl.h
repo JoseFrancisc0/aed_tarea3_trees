f972eb1666a10f5dc48672e92d1b69e2ede21521
JoseFrancisc0 <83974552+JoseFrancisc0@users.noreply.github.com>
2024-01-23 17:34:17 -0500

    AVL skeleton class

--- avl.h
+++ avl.h
@@ -1,7 +1,25 @@
 #ifndef AVL_H
 #define AVL_H
 
+#include <string>
+#include <vector>
+using namespace std;
+
 template<typename TK, typename TV>
-class AVL{};
+class AVL{
+    private:
+        struct Node{};
+        Node* root;
+    public:
+        AVL(): root(nullptr){};
+        void insert(TK key, TV value);
+        void erase(TK key);
+        vector<TV> search(TK begin, TK end);
+        void print(string traversal = "inorder");
+        void clear();
+        ~AVL(){
+            clear();
+        }
+};
 
 #endif //AVL_H
