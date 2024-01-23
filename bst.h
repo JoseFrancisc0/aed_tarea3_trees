8b4d6e19331f0f36f983931a45b967ca393f7cfc
JoseFrancisc0 <83974552+JoseFrancisc0@users.noreply.github.com>
2024-01-23 17:33:19 -0500

    BST skeleton class

--- bst.h
+++ bst.h
@@ -1,7 +1,25 @@
 #ifndef BST_H
 #define BST_H
 
+#include <string>
+#include <vector>
+using namespace std;
+
 template<typename TK, typename TV>
-class BST{};
+class BST{
+    private:
+        struct Node{};
+        Node* root;
+    public:
+        BST(): root(nullptr){};
+        void insert(TK key, TV value);
+        void erase(TK key);
+        vector<TV> search(TK begin, TK end);
+        void print(string traversal = "inorder");
+        void clear();
+        ~BST(){
+            clear();
+        }
+};
 
 #endif //BST_H
