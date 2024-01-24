4ade146c06fed71d824fa16f85fa5cb8307ac80b
JoseFrancisc0 <jose.wong@utec.edu.pe>
2024-01-24 14:26:57 -0500

    BST Tests passed, ahorita lo subo al canvas

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 #include "avl.h"
+#include "bst.h"
 using namespace std;
+
+void test_bst(){
+    BST<int> tree;
+
+    // Testing insert
+    tree.insert(16);
+    tree.insert(25);
+    tree.insert(36);
+    tree.insert(49);
+    tree.insert(1);
+    tree.insert(4);
+    tree.insert(9);
+    tree.print();
+
+    // Testing search and erase
+    cout << boolalpha << tree.search(25) << '\n';
+    tree.erase(25);
+    cout << boolalpha << tree.search(25) << "\n";
+    for(auto i : tree.range_search(15, 50))
+        cout << i << " ";
+    cout << "\n";
+
+    // Testing all traversal
+    tree.print("preorder");
+    tree.print("inorder");
+    tree.print("postorder");
+    tree.print("BFS");
+    tree.print("DFS");
+
+    // Testing clear
+    tree.clear();
+    tree.print();
+}
 
 void test_avl(){
     AVL<int> tree;
@@ -37,4 +71,5 @@ void test_avl(){
 
 int main(){
     //test_avl();
+    //test_bst();
 }
